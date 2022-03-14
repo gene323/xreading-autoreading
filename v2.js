73e93a32b958a174bd6305a62dbdf7580e372f44
gene323 <geneho0323@gmail.com>
2022-03-14 20:19:10 +0800

    fix bug

--- v2.js
+++ v2.js
@@ -1,48 +1,66 @@
-const wpm = 150;
+const wpmMin = 150;
+const wpmMax = 160;
 
 function timeToTurn(pastWord, currentWord){
-    console.log("Time to read:", parseInt( (currentWord - pastWord))/wpm * 60 );
-    return parseInt((currentWord - pastWord)/ wpm);
+    wpm = Math.floor( Math.random() * (wpmMax - wpmMin + 1) + wpmMin );
+    //Time(second)
+    let time = parseInt( (currentWord - pastWord) )/wpm * 60;
+    console.log("Time(sec) to read:", time);
+    return time;
 }
 
 function getWord(){
-    let word = parseInt(document.querySelectorAll('.activeContents')[1].getAttribute('sectioncount'));
+    let word = parseInt(
+        document.querySelectorAll('.activeContents')[1].getAttribute('sectioncount')
+    );
     console.log("Get word", word);
     return word;
 }
 
-function reading(currentWord){
+function reading(pastWord, currentWord){
 
+    let time = timeToTurn(pastWord, currentWord);
     setTimeout( () => {
-        currentWord = getWord();
-
         if(document.querySelector('button.close-book') != null){
             document.querySelcetor('button.close-book').click();
         }
         else {
             document.querySelector('button.next-slide').click();
+            console.log('Turn to next page');
             setTimeout( () => {
-                reading(currentWord);
+                pastWord = currentWord;
+                currentWord = getWord();
+                reading(pastWord, currentWord);
             }, 3000)
         }
-    }, timeToTurn(currentWord, getWord()) * 60 * 1000 );
+    }, time * 1000 );
 }
 
 function main(){
 
     setInterval( () => {
         if(document.querySelector('button.continue-reading') == null ){
-            console.log('watching');
+            console.log('close afk alert');
         }
         else{ 
             document.querySelector('button.continue-reading').click();
-            console.log('still reading');
+            console.log('reading');
         }
 
-    }, 2000);
+    }, 3000);
+
+    let pastWord = getWord();
+    if(getWord() === 0){
+        while( getWord() ){
+            document.querySelector('button.continue-reading').click();
+        }
+    }
+    else{
+        document.querySelector('button.continue-reading').click();
+    }
 
     let currentWord = getWord();
-    reading( currentWord );
+    reading(pastWord, currentWord);
     console.log('Start !');
 }
 main();
